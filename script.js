b0444d2fc87c87157d70d9c96228916d7445c931
Gitte Sevel Bilenberg <36244161+GitteSB@users.noreply.github.com>
2021-04-09 13:26:59 +0200

    Changes

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-const panels = document.querySelectorAll('panel')
+const panels = document.querySelectorAll('.panel')
 
-panels.forEach((panel) => {
-    panel.addEventListenere('click', () => {
+panels.forEach((panel => {
+    panel.addEventListener('click', () => {
         removeActiveClasses()
        panel.classList.add('acitve')
-    })
+    });
 })
+
 function removeActiveClasses(){
     panels.forEach(panel =>{
         panel.classList.remove('active')
-    })
-}
+    });
+};
